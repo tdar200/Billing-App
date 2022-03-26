0933d5a65e6d206861b5922e3a9e6eebb66409a9
Tahseen <tahseendar01@gmail.com>
2022-03-26 12:57:26 +0500

    added bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {Container} from "react-bootstrap"
+// import Table from "react-bootstrap/Table";
+// import Button from "react-bootstrap/Button";
 const App = () => {
+
+	const [logs, setLogs] = useState([
+	
+	])
+
 	return (
-		<div className='app'>
+		<Container>
 			<h1>React Electron Boilerplate</h1>
 			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
+		</Container>
 	)
 }
 
