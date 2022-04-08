675664ab126db0fe16db792b29c9e0608d62cd05
Tahseen <tahseendar01@gmail.com>
2022-04-08 21:31:34 +0500

    customer list

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -5,7 +5,60 @@ function CustomerList() {
   return (
     <div>
       <NavbarComponent />
-      CustomerList changes
+      <Table className="mt-3" responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>Invoice Id</th>
+            <th>Bill Issue Date</th>
+            <th>Due Date</th>
+            <th>Billing Month</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>CNIC</th>
+            <th>Phone No.</th>
+            <th>Internet Speed</th>
+            <th>Monthly Bill</th>
+            <th>Remainder Balance</th>
+            <th>Total Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data &&
+            data.map((item, idx) => {
+              const {
+                invoiceNumber,
+                issueDate,
+                dueDate,
+                billingMonth,
+                name,
+                email,
+                id,
+                phone,
+                internetSpeed,
+                cost,
+                prevBalance,
+                total,
+              } = item;
+
+              return (
+                <tr key={idx}>
+                  <td>{invoiceNumber}</td>
+                  <td>{new Date(issueDate).toISOString().slice(0, 10)}</td>
+                  <td>{new Date(dueDate).toISOString().slice(0, 10)}</td>
+                  <td>{billingMonth}</td>
+                  <td>{name}</td>
+                  <td>{email}</td>
+                  <td>{id}</td>
+                  <td>{phone}</td>
+                  <td>{internetSpeed}</td>
+                  <td>{cost}</td>
+                  <td>{prevBalance}</td>
+                  <td>{total}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </div>
   );
 }
